e05315998a63c92c04120b5f6b3b89726b413320
Austin Layman <61898015+EnkiEnki77@users.noreply.github.com>
2022-09-19 05:47:00 -0700

    optimized location page for desktop

--- components/Location.tsx
+++ components/Location.tsx
@@ -12,9 +12,9 @@ type Props = {
 
 const Location = (props: Props) => {
   return (
-    <figure className="md:flex md:flex-col md:gap-8 md:px-10">
-        <img className="md:rounded-2xl min-h-[320px] md:h-[326px]" src={require(`../public/assets/locations/tablet/${props.img}`).default.src} alt="" />
-        <figcaption className={`md:rounded-2xl md:gap-x-32 md:px-[75px] px-6 py-20 flex flex-col md:flex-row md:flex-wrap  items-center gap-6 min-h-[394px] md:min-h-[326px] md:h-[326px] bg-bgPattern2 bg-lightPeach/10`}>
+    <figure className="md:flex lg:basis-1/3 md:flex-col md:gap-8 md:px-10 lg:flex-row lg:[&:nth-of-type(odd)]:flex-row-reverse lg:px-[160px]">
+        <img className="md:rounded-2xl lg:w-1/3 min-h-[320px] md:h-[326px]" src={require(`../public/assets/locations/tablet/${props.img}`).default.src} alt="" />
+        <figcaption className={`md:rounded-2xl lg:flex-row lg:px-[95px] lg:basis-2/3 md:gap-x-32 md:px-[75px] px-6 py-20 flex flex-col md:flex-row md:flex-wrap  items-center gap-6 min-h-[394px] md:min-h-[326px] md:h-[326px] bg-bgPattern2 bg-lightPeach/10`}>
             <h1 className='capitalize text-peach md:basis-full'>{props.country}</h1>
             <div>
                 <p className='text-black font-bold text-center capitalize md:text-left'>{props.office}</p>
