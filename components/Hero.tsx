663be5b282bf1f5ae921d20d5f1be32039bc3465
Austin Layman <61898015+EnkiEnki77@users.noreply.github.com>
2022-12-11 16:45:30 -0800

    fixed responsiveness of project component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,25 +1,33 @@
-import React from 'react'
-import Button from './Button'
-import heroPhone from '../public/assets/home/desktop/image-hero-phone.png'
-import Link from 'next/link'
+import React from "react";
+import Button from "./Button";
+import heroPhone from "../public/assets/home/desktop/image-hero-phone.png";
+import Link from "next/link";
 
-type Props = {}
+type Props = {};
 
 const Hero = (props: Props) => {
   return (
     <div className="md:px-10  lg:px-[165px] md:pt-[155px] relative">
       <div className="lg:py-[145px] lg:w-full flex flex-col lg:flex-row md:rounded-[15px] overflow-hidden md:pt-[60px] items-center mb-[120px] lg:mb-[160px] px-6 pt-[96px] bg-[url('../public/assets/home/desktop/bg-pattern-hero-home.svg')] bg-[left_top_12.5rem] bg-peach w-full ">
-          <div className='flex flex-col items-center lg:items-start lg:px-[96px]'>
-              <h1 className="lg:pr-[350px] text-center lg:text-left mt-20 md:mt-0 md:px-10 mb-[24px] lg:mb-8 md:mb-6 lg:px-0 leading-[1.1]">Award-winning custom designs and digital branding solutions</h1>
-              <p className="lg:pr-[520px] text-center px-2 lg:text-left mb-6 lg:mb-10 md:px-[105px] lg:px-0">With over 10 years in the industry, we are experienced in creating fully responsive websites, app design, and engaging brand experiences. Find out more about our services.</p>
-              <Button path='/about' btn='buttonLight'>learn more</Button>
-          </div>
-          <div className='lg:h-full bottom-0 right-[260px] bg-transparent rounded-lg bg-heroPhone h-[450px] lg:absolute bg-no-repeat w-full lg:w-[300px] bg-[center_top_-6rem] md:bg-[center_top_5rem] z-20'>
-              {/* <img src={heroPhone.src} alt="" /> */}
-          </div>
+        <div className="flex flex-col items-center lg:items-start lg:px-[96px]">
+          <h1 className="lg:pr-[350px] text-center lg:text-left mt-20 md:mt-0 md:px-10 mb-[24px] lg:mb-8 md:mb-6 lg:px-0 leading-[1.1]">
+            Award-winning custom designs and digital branding solutions
+          </h1>
+          <p className="lg:pr-[520px] text-center px-2 lg:text-left mb-6 lg:mb-10 md:px-[105px] lg:px-0">
+            With over 10 years in the industry, we are experienced in creating
+            fully responsive websites, app design, and engaging brand
+            experiences. Find out more about our services.
+          </p>
+          <Button path="/about" btn="buttonLight">
+            learn more
+          </Button>
+        </div>
+        <div className="lg:h-full bottom-0 right-[260px] bg-transparent rounded-lg bg-heroPhone h-[450px] lg:absolute bg-no-repeat w-full lg:w-[300px] bg-[center_top_-6rem] md:bg-[center_top_-6rem] lg:bg-[center_top_7rem] z-20">
+          {/* <img src={heroPhone.src} alt="" /> */}
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
--- components/Project.tsx
+++ components/Project.tsx
@@ -1,23 +1,29 @@
-import React from 'react'
+import React from "react";
 
 type Props = {
-  img: string,
-  title: string, 
-  description: string,
-  page: string
-}
+  img: string;
+  title: string;
+  description: string;
+  page: string;
+};
 
 const Project = (props: Props) => {
-
   return (
     <figure className="h-[478px] md:h-[310px] rounded-[15px] overflow-hidden md:flex lg:w-full lg:h-full lg:flex-col">
-      <img className=" md:w-1/2 lg:w-full min-h-[310px] md:h-full lg:h-4/5" src={require(`../public/assets/${props.page}/desktop/${props.img}`).default.src} alt="" />
+      <img
+        className=" md:w-1/2 lg:w-full w-full h-full min-h-[310px] max-h-[320px] md:h-full lg:h-4/5"
+        src={
+          require(`../public/assets/${props.page}/desktop/${props.img}`).default
+            .src
+        }
+        alt=""
+      />
       <figcaption className=" md:rounded-none min-h-[158px] md:h-[310px] md:w-1/2 lg:w-full lg:h-1/5 bg-lightPeach/20 flex flex-col items-center justify-center gap-4 px-7 rounded-[15px]">
         <h3 className="uppercase text-peach text-center">{props.title}</h3>
-        <p className='text-black text-center'>{props.description}</p>
+        <p className="text-black text-center">{props.description}</p>
       </figcaption>
     </figure>
-  )
-}
+  );
+};
 
-export default Project
+export default Project;
