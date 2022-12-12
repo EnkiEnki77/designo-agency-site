b3e1e3cc9defa075c1562af09d086cba32ecd17e
Austin Layman <61898015+EnkiEnki77@users.noreply.github.com>
2022-12-11 17:00:16 -0800

    fixed responsiveness of contact form component

--- components/FormElement.tsx
+++ components/FormElement.tsx
@@ -1,16 +1,20 @@
-import React from 'react'
+import React from "react";
 
 type Props = {
-    placeholder: string,
-    type: string
-}
+  placeholder: string;
+  type: string;
+};
 
 const FormElement = (props: Props) => {
   return (
-    <div className=" max-w-[380px] md:max-w-full lg:w-full h-[38px] border-b border-white">
-        <input className="text-white active:border-b-[3px] outlne-none bg-transparent w-full h-full px-4 pb-3 placeholder:text-white/50" type={props.type} placeholder={props.placeholder} />
+    <div className=":max-w-full lg:w-full h-[38px] border-b border-white">
+      <input
+        className="text-white active:border-b-[3px] outlne-none bg-transparent w-full h-full px-4 pb-3 placeholder:text-white/50"
+        type={props.type}
+        placeholder={props.placeholder}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default FormElement
+export default FormElement;
