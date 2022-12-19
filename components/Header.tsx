7e262d226bcc35c9867f320ec43651bddb31e8ed
Austin Layman <61898015+EnkiEnki77@users.noreply.github.com>
2022-12-19 14:01:36 -0800

    created header modal animations

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,6 +6,7 @@ import Hamburger from "../public/assets/shared/mobile/icon-hamburger.svg";
 import Nav from "./Nav";
 import NavModal from "./NavModal";
 import { AnimatePresence, motion } from "framer-motion";
+import { XMarkIcon } from "@heroicons/react/24/solid";
 
 type Props = {};
 
@@ -37,7 +38,7 @@ const Header = (props: Props) => {
       transition={{ duration: 0.5 }}
       className="fixed top-0 z-30 w-full"
     >
-      <div className="bg-white lg:px-[165px] w-full py-[34px] md:py-16 flex items-center  justify-between px-6 md:px-10 fixed z-40">
+      <header className="bg-white lg:px-[165px] w-full py-[34px] md:py-16 flex items-center  justify-between px-6 md:px-10 fixed z-40">
         <Link href="/#">
           <img
             className="h-[27px] w-[202px] hover:cursor-pointer"
@@ -45,16 +46,36 @@ const Header = (props: Props) => {
             alt=""
           />
         </Link>
-        {width < 768 && (
-          <img
-            onClick={handleToggle}
-            className="hover:cursor-pointer"
-            src={Hamburger.src}
-            alt=""
-          />
+        {width < 768 && !toggle ? (
+          <AnimatePresence>
+            <motion.img
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.5 }}
+              onClick={handleToggle}
+              className="hover:cursor-pointer"
+              src={Hamburger.src}
+              alt=""
+            />
+          </AnimatePresence>
+        ) : (
+          <AnimatePresence>
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.5 }}
+            >
+              <XMarkIcon
+                onClick={handleToggle}
+                className="w-6 h-6 cursor-pointer"
+              />
+            </motion.div>
+          </AnimatePresence>
         )}
         {width >= 768 && <Nav header={true} />}
-      </div>
+      </header>
       <AnimatePresence>
         {toggle && [
           <NavModal key="nav modal" />,
